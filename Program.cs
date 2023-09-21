442c79c6eb13fbb147e5bce3cfecbb1acc609d36
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2023-09-21 14:16:43 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");    //write in console
+//Console.ReadLine();  //console will end automatically
+Console.Write("Hi");    //continue in same line
+
+
+//comments
+
+//single line comment
+/* multi line comment 
+1
+2
+3
+*/
+
+
+//variables and data types
+
+int a=10;
+int b=29;
+Console.WriteLine(a+b);
+float f=3.0F;
+bool isKing=true;
+string imp=Console.ReadLine();
+Console.WriteLine(imp);
+
+
