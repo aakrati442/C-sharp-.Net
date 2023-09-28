ccf08bd13efe4b45bac646ac00e0e6dab66e0369
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2023-09-28 16:38:08 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");    //write in console
+// Console.WriteLine("Hello, World!");    //write in console
 //Console.ReadLine();  //console will end automatically
-Console.Write("Hi");    //continue in same line
+// Console.Write("Hi");    //continue in same line
 
 
 //comments
@@ -28,13 +28,28 @@ Console.Write("Hi");    //continue in same line
 // string imp=Console.ReadLine();
 // Console.WriteLine(imp);
 
-Console.WriteLine("Enter your name");
-string name=Console.ReadLine();
-Console.WriteLine("Hello "+ name);
+// Console.WriteLine("Enter your name");
+// string name=Console.ReadLine();
+// Console.WriteLine("Hello "+ name);
 
-int d=0;
-Console.WriteLine(int.MinValue);
-
-
+// int d=0;
+// Console.WriteLine(int.MinValue);
 
 
+//Methods
+
+// using System;
+
+// class Program{
+//     public static void Main(){
+//         Program p=new Program();
+//         p.EvenNum();
+//     }
+//     public void EvenNum(){
+//         int start=0;
+//         while(start<=20){
+//             Console.WriteLine(start);
+//             start+=2;
+//         }
+//     }
+// }
